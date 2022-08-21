fcab1c454246be85f0a571e3417cfd4b2f3522c2
Sutopa <sutopa28dec@gmail.com>
2022-08-21 11:50:02 -0700

    header added

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCi3CvPc9pMRSSCjWLUQbmr1EuurZZNmY8",
+    authDomain: "challenge-3a52e.firebaseapp.com",
+    projectId: "challenge-3a52e",
+    storageBucket: "challenge-3a52e.appspot.com",
+    messagingSenderId: "377522865450",
+    appId: "1:377522865450:web:c9763f6030bce9f67d8a53",
+    measurementId: "G-ZZSEDE22X3"
+  };
